c4aed52672fb23361443df16cb6df3285610b6ee
NickDimarzo <124927183+NickDimarzo@users.noreply.github.com>
2024-02-23 11:57:12 -0700

    navigation sorted

--- App.jsx
+++ App.jsx
@@ -18,79 +18,24 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-import {useNavigation} from '@react-navigation/native';
-import Orientation from 'react-native-orientation-locker';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './screens/Home';
+import SceneOne from './screens/SceneOne';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
-  // Lock the orientation to landscape
-  Orientation.lockToLandscape();
-
-  // Get the navigation object
-  const navigation = useNavigation();
-
-  // alerts the user when the button is pressed
- const _onPressButton = () => {
-    navigation.navigate('');
-  };
-
   return (
-    <SafeAreaView style={styles.backgroundColor}>
-      <View>
-        <Text style={styles.title}>Cyber Strike</Text>
-        <View style= {styles.container}>
-          <Image
-            source={require('./assets/images/title_image.jpg')}
-            style={styles.image}
-          />
-        <TouchableHighlight onPress={_onPressButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Start Game</Text>
-          </View>
-        </TouchableHighlight>
-        <Image
-            source={require('./assets/images/title_image.jpg')}
-            style={[styles.image, {transform: [{scaleX: -1}]}]}
-          />
-        </View>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="SceneOne" component={SceneOne} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
 
-const styles = StyleSheet.create({
-  backgroundColor: {
-    backgroundColor: '#d3d3d3',
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: 'black',
-    backgroundColor : 'white',
-  },
-  image: {
-    width: 200,
-    height: 300,
-    backgroundColor: '#d3d3d3',
-  },
-  button: {
-    backgroundColor: 'blue',
-    padding: 10,
-    margin: 20,
-    borderRadius: 10,
-    textAlign: 'center',
-  },
-  buttonText: {
-    fontSize: 30,
-    color: 'white',
-    textAlign: 'center',
-  },
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  
-  },
-});
+
+}
 
 export default App;
---
+++ screens/SceneOne.jsx
@@ -0,0 +1,54 @@
+import react from 'react';
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  useColorScheme,
+  View,
+  ImageBackground,
+} from 'react-native';
+import Orientation from 'react-native-orientation-locker';
+import {useNavigation} from '@react-navigation/native';
+
+export default function SceneOne() {
+  // Lock the orientation to landscape
+  Orientation.lockToLandscape();
+
+  const navigation = useNavigation();
+
+  return (
+    <ImageBackground
+      source={require('../assets/images/scene_one_backround.png')}
+      style={{flex: 1}}>
+      <Pressable onPress={() => navigation.navigate('Home')}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Action 1</Text>
+        </View>
+      </Pressable>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  backgroundColor: {
+    backgroundColor: '#d3d3d3',
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    margin: 20,
+    borderRadius: 10,
+    textAlign: 'center',
+    width: 100,
+  },
+  buttonText: {
+    fontSize: 10,
+    color: 'white',
+    textAlign: 'center',
+  },
+});
